05c1693ae25d96497bc71585eb1c746098a9c9ec
Diêgo de Barros <debarrosdiego415@gmail.com>
2024-07-02 10:03:02 -0300

    update3

--- server.js
+++ server.js
@@ -2,16 +2,14 @@ const express = require("express");
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 app.use(
-  "/proxy",
+  "/",
   createProxyMiddleware({
     target: "https://www.altogiro.com.br",
     changeOrigin: true,
-    pathRewrite: {
-      "^/proxy": "",
-    },
+
     onProxyReq: (proxyReq, req, res) => {
       proxyReq.removeHeader("X-Frame-Options");
       proxyReq.removeHeader("Content-Security-Policy");
