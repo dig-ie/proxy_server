3443d849365c2da6e52f86e8266b2bab9c7d3c8e
Diêgo de Barros <debarrosdiego415@gmail.com>
2024-07-02 09:52:43 -0300

    update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(
   "/proxy",
